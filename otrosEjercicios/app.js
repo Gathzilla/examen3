67e95c9191bb1a2745c1beb68061a68b73dcf627
Gerardo Tzoc <gath2-96@hotmail.com>
2019-08-16 23:46:47 -0600

    added provider excercise

--- otrosEjercicios/app.js
+++ otrosEjercicios/app.js
@@ -11,3 +11,32 @@ module.controller("redesController",["$scope","redes",function($scope,redes) {
   $scope.segundaRed = redes[1].red + " : " + redes[1].url;  
   $scope.terceraRed = redes[2].red + " : " + redes[2].url;  
 }]);
+
+
+module.provider('preferences', function () {
+
+    this.$get = function () {
+        let preferencesArray = {
+
+            "language" : "EN",
+            "theme": "indigo",
+            "style" : "Modern"
+        };
+        
+        return {
+            userPreferences: preferencesArray
+        };
+    }
+});
+
+module.config(function (preferencesProvider) {
+   
+});
+
+module.controller("preferencias",function ($scope, preferences) {
+
+    $scope.preferenceOne = preferences.userPreferences.language;
+    $scope.preferenceTwo = preferences.userPreferences.theme;
+    $scope.preferenceThree = preferences.userPreferences.style;
+
+});
